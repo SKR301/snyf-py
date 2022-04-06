baf1fe86cf632ef63d95a5ae64b24577fcb1a505
SKR301 <chunnu.bihat@gmail.com>
2022-04-07 00:09:21 +0530

    #2 prepared dict for heat map

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from scapy import all as scapy
 import pygeoip
+import datetime
+import traceback
 import sys
+from os import system
 
-interface = sys.argv[1]
+# interface = sys.argv[1]
+interface = 'Ethernet'
 GEO = pygeoip.GeoIP('GeoIP.dat')
+heatMapSrcData = {}
+heatMapDstData = {}
 
 def getCountryFromIP(ip):
     return GEO.country_name_by_addr(ip)
@@ -15,7 +21,23 @@ while True:
         srcIP = pkt[0].getlayer("IP").src
         dstIP = pkt[0].getlayer("IP").dst
 
-        print(srcIP,":",getCountryFromIP(srcIP),"-",dstIP,getCountryFromIP(dstIP))
+        srcCountry = 'India' if srcIP[:3] == '192' else getCountryFromIP(srcIP)
+        dstCountry = 'India' if dstIP[:3] == '192' else getCountryFromIP(dstIP)
+
+        if srcCountry in heatMapSrcData:
+            heatMapSrcData[srcCountry] = heatMapSrcData[srcCountry]+1
+        else:
+            heatMapSrcData[srcCountry] = 1
+        
+        if dstCountry in heatMapDstData:
+            heatMapDstData[dstCountry] = heatMapDstData[dstCountry]+1
+        else:
+            heatMapDstData[dstCountry] = 1
+
+        # print(srcIP, ':', srcCountry, '-', dstIP, ':', dstCountry, '[', datetime.datetime.now(), ']')
+        # print('src:\t', heatMapDstData)
+        # print('dst:\t', heatMapSrcData)
+
     except Exception:
-        # print(traceback.format_exc())
+        print(traceback.format_exc())
         print()
