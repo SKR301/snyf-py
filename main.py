0a36f8974df5a84f6e1d43e45b40ce94884bc58e
SKR301 <chunnu.bihat@gmail.com>
2022-04-06 22:11:01 +0530

    #1 capture src and dst

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from scapy import all as scapy
 import sys
 
 interface = sys.argv[1]
-scapy.sniff(iface = interface, prn = lambda x: x.summary())
+while True:
+    pkt = scapy.sniff(count =1, iface = interface, prn = lambda x: x.summary())
+    
+    try:
+       srcIP = pkt[0].getlayer("IP").src
+       dstIP = pkt[0].getlayer("IP").dst
+    except:
+        print()
