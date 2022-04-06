973cab0fb8f1bdf68414ce200dec0db4ba68b368
SKR301 <chunnu.bihat@gmail.com>
2022-04-06 23:44:05 +0530

    #2 extracted country name from IP

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from scapy import all as scapy
+import pygeoip
+import traceback
 import sys
 
 interface = sys.argv[1]
+GEO = pygeoip.GeoIP('GeoIP.dat')
+
+def getCountryFromIP(ip):
+    return GEO.country_name_by_addr(ip)
+
 while True:
-    pkt = scapy.sniff(count =1, iface = interface, prn = lambda x: x.summary())
+    pkt = scapy.sniff(count =1, iface = interface)
     
     try:
-       srcIP = pkt[0].getlayer("IP").src
-       dstIP = pkt[0].getlayer("IP").dst
-    except:
+        srcIP = pkt[0].getlayer("IP").src
+        dstIP = pkt[0].getlayer("IP").dst
+
+        print(srcIP,":",getCountryFromIP(srcIP),"-",dstIP,getCountryFromIP(dstIP))
+    except Exception:
+        # print(traceback.format_exc())
         print()
