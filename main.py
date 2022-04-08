dc143fe73b4c677c054d1684f72acea2f063c34b
SKR301 <chunnu.bihat@gmail.com>
2022-04-08 21:07:05 +0530

    #1 creating hash map for src/dst cc

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from scapy import all as scapy
 import read_ip_csv as ric
 import datetime
 import os
+import numpy as np
 
 # INTERFACE = sys.argv[1]
 INTERFACE = 'Ethernet'
 LOCAL_IPS = []
+src_loc_count = {}
+dst_loc_count = {}
+
+def normalizeDict(input_dict):
+    X = np.array([val for val in input_dict.values()])
+    norm_1 = np.abs(X).sum()
+    return {key : input_dict[key]/norm_1 for key in input_dict.keys()}
+
+def updateDict(key, input_dict):
+    if key in input_dict:
+        input_dict[key] = 1
+    else:
+        input_dict[key] = input_dict[key]+1
+    return input_dict
 
 def snyf():
     while True:
@@ -18,9 +33,13 @@ def snyf():
             srcCC = 'LOCAL' if srcIP in LOCAL_IPS else ric.getCountryCodeFromIP(srcIP)
             dstCC = 'LOCAL' if dstIP in LOCAL_IPS else ric.getCountryCodeFromIP(dstIP)
 
-            print(srcIP, '\t', srcCC, '\t', dstIP, '\t', dstCC, '\t', datetime.datetime.now())
-        except Exception:
+            updateDict(srcCC, src_loc_count)
+            updateDict(dstCC, dst_loc_count)
+
+            # print(srcIP, '\t', srcCC, '\t', dstIP, '\t', dstCC, '\t', datetime.datetime.now())
             print()
+        except Exception as e: 
+            print(e)
 
 def setLocalIP():
     IPs = os.popen('ipconfig | findstr IPv4').read().split('\n')[:-1]
